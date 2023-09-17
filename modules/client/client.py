408c78b6ef7aef3521c71db1a7ca760cdc17e892
FarrOut <4e4f9ce1@opayq.com>
2023-09-17 14:50:41 +0200

    trying to get timezone correct

--- modules/client/client.py
+++ modules/client/client.py
@@ -2,29 +2,30 @@ import json
 import datetime
 import requests
 import logging
-
+import pytz
 from urllib3.exceptions import NewConnectionError
 
 
 class Client:
+    # ISO 8601 - YYYY-MM-DDTHH:MM:SSZ
+    # E.g "2010-04-14T02:15:15Z"
+    time_format = "%Y-%m-%dT%H:%M:%S"
+
+    __url_prefix = 'https://api.github.com'
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
 
     def __init__(self, access_token: str, ):
         self.token = access_token
-        self.__url_prefix = 'https://api.github.com'
-        self.logger = logging.getLogger()
-        self.logger.setLevel(logging.DEBUG)
+
 
     def __put_last_access_timestamp(self):
         # TODO refactor __put_last_access_timestamp and __get_last_access_timestamp to reuse common code
 
-        date_obj = datetime.datetime.now()
-        # timestamp = date_obj.strftime("%G-%m-%dT%H:%M:%S%Z")
-        # ISO 8601 - YYYY-MM-DDTHH:MM:SSZ
-
-        timestamp = date_obj.isoformat()
-        timestamp = "2010-04-14T02:15:15Z"
-        timestamp = date_obj.timestamp()
-
+        date_obj = datetime.datetime.now(tz=pytz.UTC)
+        # tz = pytz.timezone('Asia/Kolkata')
+        # tz.localize(date_obj)
+        timestamp = date_obj.strftime(Client.time_format)
 
         dictionary = {
             'last_accessed': f'{timestamp}'
@@ -32,7 +33,7 @@ class Client:
         json_object = json.dumps(dictionary, indent=4)
 
         file_name = "gistory.config.json"
-        self.logger.debug(f'Recording current access timestamp of {timestamp} to {file_name}')
+        Client.logger.debug(f'Recording current access timestamp of {timestamp} to {file_name}')
         with open(file_name, "w") as outfile:
             outfile.write(json_object)
 
@@ -46,19 +47,21 @@ class Client:
                 json_object = json.load(openfile)
                 last_accessed = json_object['last_accessed']
 
-                self.logger.debug(f'Found last access timestamp of \'{last_accessed}\' in {file_name}')
-                timestamp = datetime.date.fromtimestamp(float(last_accessed))
+                Client.logger.debug(f'Found last access timestamp of \'{last_accessed}\' in {file_name}')
+                timestamp = datetime.datetime.strptime(last_accessed, self.time_format)
 
                 return timestamp
 
+        except KeyError as err:
+            Client.logger.info('Did not find last access timestamp.')
         except ValueError as err:
-            self.logger.error('Failed to parse last access timestamp!', exc_info=err)
+            Client.logger.error('Failed to parse last access timestamp!', exc_info=err)
         except Exception as err:
-            self.logger.error('Failed to retrieve last access timestamp!', exc_info=err)
+            Client.logger.error('Failed to retrieve last access timestamp!', exc_info=err)
 
     def __submit_request(self, endpoint: str, params: dict = {}):
-        __url = f'{self.__url_prefix}/{endpoint}'
-        self.logger.debug(f'Submitting request to {__url}...')
+        __url = f'{Client.__url_prefix}/{endpoint}'
+        Client.logger.debug(f'Submitting request to {__url}...')
         print(f'Submitting request to {__url}...')  # TODO deleteme
 
         try:
@@ -73,16 +76,17 @@ class Client:
                 return response.json()
 
         except (ConnectionError, requests.exceptions.ConnectionError, NewConnectionError) as connErr:
-            self.logger.error('Request failed to establish connection!', exc_info=connErr)
+            Client.logger.error('Request failed to establish connection!', exc_info=connErr)
         except Exception as err:
-            self.logger.error('Request failed!', exc_info=err)
+            Client.logger.error('Request failed!', exc_info=err)
 
     def __output_gists(self, json_: dict):
 
         for idx, gist_ in enumerate(json_):
             print(f'Gist #{str(idx)}:\n{self.__parse_gist(gist_)}')
 
-    def __parse_gist(self, gist_json: dict) -> str:
+    @staticmethod
+    def __parse_gist(gist_json: dict) -> str:
 
         id_ = gist_json["id"]
         created_at = gist_json["created_at"]
@@ -98,19 +102,31 @@ class Client:
 
         return parsed_gist_
 
-    def list_gists(self):
-        self.logger.debug('Listing all Gists')
-        timestamp = self.__get_last_access_timestamp()
+    @staticmethod
+    def __parse_timestamp(datetime_obj: datetime) -> str:
 
-        if timestamp is not None:
-            self.logger.debug(f'Found previous access timestamp of {timestamp.isoformat()}.')
-        else:
-            self.logger.info(f'Didn\'t find last access timestamp - presuming this is first-time run..')
+        timestamp = datetime_obj.strftime(Client.time_format)
+        # timestamp = "2010-04-14T02:15:15Z"
+        parsed_timestamp = f'{timestamp}'
+
+        return parsed_timestamp
+
+    def list_gists(self):
+        Client.logger.debug('Listing all Gists')
 
         params = {"per_page": 30}
 
+        timestamp = self.__get_last_access_timestamp()
+
+        if timestamp is not None:
+            timestamp_formatted = self.__parse_timestamp(timestamp)
+            Client.logger.debug(f'Only listing Gists last updated since {timestamp_formatted}.')
+            params['since'] = timestamp_formatted
+        else:
+            Client.logger.info(f'Didn\'t find last access timestamp - presuming this is first-time run..')
+
         json_ = self.__submit_request(endpoint='gists', params=params)
-        self.logger.debug(f'list_gist found {len(json_)} items.')
+        Client.logger.debug(f'list_gist found {len(json_)} items.')
         # self.logger.debug(json.dumps(json_, sort_keys=True, indent=4))
 
         self.__output_gists(json_)
